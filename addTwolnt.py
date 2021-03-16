3aee3074bda0611b7d5292ae0ba48b34a1c6116c
dominique-17 <dtangaevengue@gmail.com>
2021-03-16 16:53:09 +0400

    utilisation de la fonction input si l'utilisateur ne renseigne pas tous les parametres

--- addTwolnt.py
+++ addTwolnt.py
@@ -11,9 +11,17 @@ if len(sys.argv) >= 4 :
     print('Vous avez renseigne plus de 2 parametres')
     conti = 0
 
-if len(sys.argv) <= 2 :
+if len(sys.argv) < 3 :
     print('Vous avez renseigne moins de 2 parametres')
     conti = 0
+    if len(sys.argv) == 2:
+        num2 = input(" entrer le nombre 2: ")
+        print("La somme est : ",add_num(int( sys.argv[1] ),num2))
+    if len(sys.argv) == 1:
+        num1 = input(" entrer le nombre 1: ")
+        num2 = input(" entrer le nombre 2: ")
+        print("La somme est : ",add_num(num1,num2))
+    
 
 if conti == 1: #ceci est pour prevenir l'erreur qui peut se creer si nous avons moins de parametres,
     numa = int( sys.argv[1] )
