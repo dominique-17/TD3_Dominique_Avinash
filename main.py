b2728e8da72f72b0e4e0931cf52704f6737eec66
dominique-17 <dtangaevengue@gmail.com>
2021-03-20 11:00:29 +0400

    creation fichier main pour choix entre addition et multuplication

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def main():
+    print("Faite le choix de votre operation 1 ou 2 ?")
+    print("1 pour Addition")
+    choix = input(" Votre choix: ")
+    if choix == 1:
+        from addTwolnt import main
+
+main()
