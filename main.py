74adad41d2fa100f3a5441a72671141825fd55b1
Avinash5483 <ahbabajee@student.udm.ac.mu>
2021-03-20 11:52:17 +0400

    import multiplication function on main file

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ import sys
 def main():
     print("Faite le choix de votre operation 1 ou 2 ?")
     print("1 pour Addition")
+    print("2 pour Multiplication") 
     choix = input(" Votre choix: ")
     if choix == 1:
         from addTwolnt import main
+    if choix == 2:
+	from mulTwoInt import mul
 
 main()
